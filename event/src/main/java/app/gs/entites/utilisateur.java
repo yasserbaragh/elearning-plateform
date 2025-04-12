34be02fd66be1092e28af781442b70a6fbdc1243
seryne204 <serynemahfoufi@gmail.com>
2025-04-12 00:54:49 +0000

    authentification ajouter

--- event/src/main/java/app/gs/entites/utilisateur.java
+++ event/src/main/java/app/gs/entites/utilisateur.java
@@ -1,20 +1,78 @@
 package app.gs.entites;
 
-
-
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
-@Table(name ="utilisateur")
-public class utilisateur {
-    @Id
+@Table(name = "utilisateur")
+public class utilisateur implements UserDetails {
+
+    public Long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNumTel() {
+		return numTel;
+	}
+
+	public void setNumTel(String numTel) {
+		this.numTel = numTel;
+	}
+
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUser;
     
@@ -24,61 +82,48 @@ public class utilisateur {
     private String username;
     private String email;
     private String numTel;
-	public utilisateur(Long idUser, String nom, String prenom, String motDePasse, String username, String email,
-			String numTel) {
-		super();
-		this.idUser = idUser;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.motDePasse = motDePasse;
-		this.username = username;
-		this.email = email;
-		this.numTel = numTel;
+    @Transient // Ce champ n’est pas stocké en base
+    private String token;
+	public String getToken() {
+		return token;
 	}
-	public utilisateur() {
-		super();
+
+	public void setToken(String token) {
+		this.token = token;
 	}
-	public Long getIdUser() {
-		return idUser;
-	}
-	public void setIdUser(Long idUser) {
-		this.idUser = idUser;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	public String getMotDePasse() {
-		return motDePasse;
-	}
-	public void setMotDePasse(String motDePasse) {
-		this.motDePasse = motDePasse;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getNumTel() {
-		return numTel;
-	}
-	public void setNumTel(String numTel) {
-		this.numTel = numTel;
-	}
+
+    // Constructeurs, getters et setters...
+
+    // Méthodes UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return motDePasse;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
     
+    // Getters/Setters standards...
 }
