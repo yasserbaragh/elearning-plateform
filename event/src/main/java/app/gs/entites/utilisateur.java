14362e43f38c671f80264bc89a5780a6f3540394
seryne204 <serynemahfoufi@gmail.com>
2025-04-12 00:05:46 +0000

    Update utilisateur.java

--- event/src/main/java/app/gs/entites/utilisateur.java
+++ event/src/main/java/app/gs/entites/utilisateur.java
@@ -5,125 +5,125 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
-@Table(name ="utilisateur")
-public class utilisateur {
-    @Id
+@Table(name = "utilisateur")
+public class utilisateur implements UserDetails {
+
+    public Long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNumTel() {
+		return numTel;
+	}
+
+	public void setNumTel(String numTel) {
+		this.numTel = numTel;
+	}
+
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUser;
-
+    
     private String nom;
     private String prenom;
     private String motDePasse;
     private String username;
     private String email;
     private String numTel;
+    @Transient // Ce champ n’est pas stocké en base
+    private String token;
+	public String getToken() {
+		return token;
+	}
 
-    // Gamification fields with default initialization
-    private int xp = 0;
-    private int lvl = 0;
-    private String badges = null; // Explicitly null (optional, default behavior)
+	public void setToken(String token) {
+		this.token = token;
+	}
 
-    public utilisateur(Long idUser, String nom, String prenom, String motDePasse, String username, String email,
-                       String numTel, int xp, int lvl, String badges) {
-        super();
-        this.idUser = idUser;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.motDePasse = motDePasse;
-        this.username = username;
-        this.email = email;
-        this.numTel = numTel;
-        this.xp = xp;
-        this.lvl = lvl;
-        this.badges = badges;
+    // Constructeurs, getters et setters...
+
+    // Méthodes UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
     }
 
-    public utilisateur() {
-        super();
-        this.xp = 0;
-        this.lvl = 0;
-        this.badges = null;
-    }
-
-    public Long getIdUser() {
-        return idUser;
-    }
-
-    public void setIdUser(Long idUser) {
-        this.idUser = idUser;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getMotDePasse() {
+    @Override
+    public String getPassword() {
         return motDePasse;
     }
 
-    public void setMotDePasse(String motDePasse) {
-        this.motDePasse = motDePasse;
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
-    public String getUsername() {
-        return username;
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
     }
 
-    public String getEmail() {
-        return email;
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNumTel() {
-        return numTel;
-    }
-
-    public void setNumTel(String numTel) {
-        this.numTel = numTel;
-    }
-
-    public int getXp() {
-        return xp;
-    }
-
-    public void setXp(int xp) {
-        this.xp = xp;
-    }
-
-    public int getLvl() {
-        return lvl;
-    }
-
-    public void setLvl(int lvl) {
-        this.lvl = lvl;
-    }
-
-    public String getBadges() {
-        return badges;
-    }
-
-    public void setBadges(String badges) {
-        this.badges = badges;
-    }
+    
+    // Getters/Setters standards...
 }
